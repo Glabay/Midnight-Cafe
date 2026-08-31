ee35ebbbd4474398c270bf1850b3d94f2b7fc79f
Glabay <Java_Guru@live.com>
2026-08-31 11:15:02 -0300

    tests and message fixes

--- src/test/java/dev/midnightcoder/cafe/MidnightCafeApplicationTests.java
+++ src/test/java/dev/midnightcoder/cafe/MidnightCafeApplicationTests.java
@@ -2,12 +2,19 @@ package dev.midnightcoder.cafe;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.modulith.core.ApplicationModules;
 
-@SpringBootTest
+@SpringBootTest(classes = MidnightCafeApplication.class)
 class MidnightCafeApplicationTests {
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void verifiesModulithArchitecture() {
+        var modules = ApplicationModules.of(MidnightCafeApplication.class).verify();
+
+        IO.println(modules.toString());
+    }
 }
